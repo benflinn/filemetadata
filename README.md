9df0c7571d56e0ca35891a99312f15730619f3e7
ben <ben.f.linn@gmail.com>
2016-11-11 18:34:40 -0600

    all features finished

---
+++ README.md
@@ -0,0 +1,5 @@
+# filemetadata
+
+This simple node server allows you to upload a single file, and then tells you how big the file is in bytes.
+
+This server uses multer, an npm module that adds a body object and a file object to the request object.
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+var multer = require('multer');
+
+var upload = multer().single('uploadedFile');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+/*
+app.post('/fileUpload', upload.single('uploadedFile'), function (req, res, next) {
+  console.log(req.file);
+  res.send(req.file);
+})
+*/
+app.post('/fileUpload', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      return
+    }
+    console.log('receiving data');
+    res.send('your file is ' + req.file.size + ' bytes.');
+
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
